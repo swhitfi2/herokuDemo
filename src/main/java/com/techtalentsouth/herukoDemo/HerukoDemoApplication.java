616d51436e76fd2a06463d49acb7260bf45f482a
Stacey Whitfield <staceywhitfield@Staceys-MacBook.local>
2019-01-22 10:05:24 -0500

    initial commit

---
+++ src/main/java/com/techtalentsouth/herukoDemo/HerukoDemoApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.herukoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerukoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerukoDemoApplication.class, args);
+	}
+
+}
+
